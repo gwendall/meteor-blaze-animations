32ad56e9b2ae83f92975a64ccbdf5dfb629afa30
Gwen <gwendall.esnault@gmail.com>
2015-04-29 20:47:30 +0200

    option to animate inital elements

--- lib.js
+++ lib.js
@@ -2,6 +2,38 @@ var getTplName = function(tpl) {
   return tpl.viewName.slice(-(tpl.viewName.length - "Template.".length));
 }
 
+var animateIn = function(classIn, element) {
+  // Hide the element before inserting to avoid a flickering when applying the "in" class
+  element.hide().removeClass(classIn);
+  Tracker.afterFlush(function() {
+    element.show().addClass(classIn);
+  });
+}
+
+var animateOut = function(classOut, element) {
+  element.addClass(classOut);
+  element.onAnimationEnd(function(animationName) {
+    element.remove();
+  });
+}
+
+var animateInitialElements = function(tplName, animations) {
+  _.each(animations, function(attrs, selector) {
+    if (!attrs.animateInitial) return;
+    Template[tplName].onRendered(function() {
+      $(selector, attrs.container).each(function(i) {
+        var element = $(this);
+        var timeout = attrs.animateInitialStep * i || 0;
+        element.hide();
+        Meteor.setTimeout(function() {
+          element.show();
+          animateIn(attrs.in, element);
+        }, timeout);
+      });
+    });
+  });
+}
+
 var getUiHooks = function(animations) {
   var hooks = {};
   _.each(animations, function(attrs, selector) {
@@ -9,19 +41,14 @@ var getUiHooks = function(animations) {
       container: attrs.container,
       insert: function(node, next) {
         var $node = $(node);
-        var opacity = $node.css("opacity") || 1; // Set the opacity to 0 before inserting the element to avoid a flickering when applying the "in" class
-        $node.removeClass(attrs.in).insertBefore(next).css({ opacity: 0 });
-        Tracker.afterFlush(function() {
-          $node.css({ opacity: opacity }).addClass(attrs.in);
-        });
+        $node.insertBefore(next);
+        animateIn(attrs.in, $node);
       },
       remove: function(node) {
         var $node = $(node);
         if (!attrs.out) return $node.remove();
-        $node.removeClass(attrs.in).addClass(attrs.out);
-        $node.onAnimationEnd(function(animationName) {
-          $node.remove();
-        });
+        $node.removeClass(attrs.in);
+        animateOut(attrs.out, $node);
       }
     }
   });
@@ -33,4 +60,5 @@ Template.prototype.animations = function(animations) {
   var tplName = getTplName(tpl);
   var hooks = getUiHooks(animations);
   Template[tplName].uihooks(hooks);
+  animateInitialElements(tplName, animations);
 };
--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "gwendall:template-animations",
   summary: "Simple DOM animations for Meteor",
   git: "https://github.com/gwendall/meteor-template-animations.git",
-  version: "0.1.0"
+  version: "0.1.1"
 });
 
 Package.onUse(function (api, where) {
