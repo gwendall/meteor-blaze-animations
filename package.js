23c2fb5ae59c83acfe05490e41e20ccf3f000103
Gwen <gwendall.esnault@gmail.com>
2015-04-30 13:22:49 +0200

    ui-hooks dep updated

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "gwendall:template-animations",
   summary: "Simple DOM animations for Meteor",
   git: "https://github.com/gwendall/meteor-template-animations.git",
-  version: "0.1.3"
+  version: "0.1.4"
 });
 
 Package.onUse(function (api, where) {
@@ -12,7 +12,7 @@ Package.onUse(function (api, where) {
     "templating@1.1.1",
     "jquery@1.11.3_2",
     "tracker@1.0.7",
-    "gwendall:ui-hooks@0.1.1",
+    "gwendall:ui-hooks@0.1.2",
     "gwendall:jquery-animation-callback@0.1.0"
   ], "client");
 
